5b06209b7ad3c839be0469a5119b171f1bbfab5b
Evgeniy Shapov <shapovich@gmail.com>
2026-02-26 13:45:55 +0800

    create modules, controllers and services for Schedule and Rooms entities

---
+++ src/rooms/rooms.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+import { RoomsService } from './rooms.service';
+
+@Controller('rooms')
+export class RoomsController {
+    constructor(private roomsService: RoomsService) {
+
+    }
+}
